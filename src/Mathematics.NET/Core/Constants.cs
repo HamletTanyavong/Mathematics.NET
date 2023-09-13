977f3ff5c0e0c89674d7f7b9cbc1cd126eac522d
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2023-09-13 01:46:36 -0500

    Create Constants.cs

---
+++ src/Mathematics.NET/Core/Constants.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="Constants.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023 Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+namespace Mathematics.NET.Core;
+
+/// <summary>Common mathematical constants</summary>
+public static class Constants
+{
+    /// <summary>Represents the natural logarithmic base, e</summary>
+    public const double E = 2.71828182845904523;
+    /// <summary>Represents the ratio of the circumference of a circle to its diameter, π</summary>
+    public const double Pi = 3.14159265358979323;
+    /// <summary>Represents π/2</summary>
+    public const double PiHalf = 1.57079632679489661;
+    /// <summary>Represents π squared</summary>
+    public const double PiSquared = 9.86960440108935861;
+    /// <summary>Represents 2π</summary>
+    public const double Tau = 6.28318530717958647;
+    /// <summary>Represents the Euler-Mascheroni constant, γ</summary>
+    public const double EulerMascheroni = 5.77215664901532860e-1;
+    /// <summary>Represents the golden ratio, φ</summary>
+    public const double GoldenRatio = 1.61803398874989484;
+    /// <summary>Represents the natural logarithm of 2</summary>
+    public const double Ln2 = 6.93147180559945309e-1;
+    /// <summary>Represents the natural logarithm of 3</summary>
+    public const double Ln3 = 1.09861228866810969;
+    /// <summary>Represents the natural logarithm of 4</summary>
+    public const double Ln4 = 1.38629436111989061;
+    /// <summary>Represents the natural logarithm of 5</summary>
+    public const double Ln5 = 1.60943791243410037;
+    /// <summary>Represents the natural logarithm of 10</summary>
+    public const double Ln10 = 2.30258509299404568;
+    /// <summary>Represents the square root of 2</summary>
+    public const double Sqrt2 = 1.41421356237309504;
+    /// <summary>Represents the square root of 3</summary>
+    public const double Sqrt3 = 1.73205080756887729;
+    /// <summary>Represents the square root of 5</summary>
+    public const double Sqrt5 = 2.23606797749978969;
+}
