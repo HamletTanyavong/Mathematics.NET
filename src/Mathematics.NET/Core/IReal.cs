b3f2ae45593dc174a0c17483fcc69dd3992ac561
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2023-09-21 16:36:22 -0500

    Update IReal and Real
    - Inherit IConstants from IReal instead of Real since Rational cant implement it

--- src/Mathematics.NET/Core/IReal.cs
+++ src/Mathematics.NET/Core/IReal.cs
@@ -39,7 +39,6 @@ public interface IReal<T, U>
       IInequalityRelations<T, bool>,
       IDecrementOperation<T>,
       IIncrementOperation<T>,
-      IConstants<T, U>,
       IComparable,
       IComparable<T>,
       IMinMaxValue<T>
