73dd2386a512aaf913e48ebd93ed271d6d530059
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2023-10-12 13:08:10 -0500

    Update Vector4.cs
    - Remove using directive

--- src/Mathematics.NET/LinearAlgebra/Vector4.cs
+++ src/Mathematics.NET/LinearAlgebra/Vector4.cs
@@ -30,7 +30,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Mathematics.NET.Core;
-using Mathematics.NET.DifferentialGeometry;
 using Mathematics.NET.LinearAlgebra.Abstractions;
 
 namespace Mathematics.NET.LinearAlgebra;
