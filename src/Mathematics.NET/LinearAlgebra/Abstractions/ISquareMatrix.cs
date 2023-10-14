417faa872e2810a760340f8a23c6f161fb7a252f
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2023-10-13 20:12:31 -0500

    Create ISquareMatrix.cs

---
+++ src/Mathematics.NET/LinearAlgebra/Abstractions/ISquareMatrix.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="ISquareMatrix.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023 Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+using Mathematics.NET.Core;
+
+namespace Mathematics.NET.LinearAlgebra.Abstractions;
+
+/// <summary>Defines support for square matrices</summary>
+/// <typeparam name="T">A type that implements <see cref="ITwoDimensionalArrayRepresentable{T, U}"/></typeparam>
+/// <typeparam name="U">A type that implements <see cref="IComplex{T}"/></typeparam>
+public interface ISquareMatrix<T, U>
+    where T : ITwoDimensionalArrayRepresentable<T, U>
+    where U : IComplex<U>
+{
+    /// <summary>Represents a value that is not a matrix</summary>
+    /// <remarks>This result will be returned when trying to invert a singular matrix</remarks>
+    static abstract T NaM { get; }
+
+    /// <summary>Compute the determinant of the matrix</summary>
+    /// <returns>The determinant</returns>
+    U Determinant();
+
+    /// <summary>Compute the inverse of the matrix</summary>
+    /// <returns>The inverse if the matrix is invertible; otherwise, <see cref="NaM"/></returns>
+    T Inverse();
+
+    /// <summary>Compute the trace of the matrix</summary>
+    /// <returns>The trace</returns>
+    U Trace();
+}
