750f94486c3943736f7525dfeba74a36c9bf92b9
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2023-10-27 06:53:28 -0500

    Create Index.cs

---
+++ src/Mathematics.NET/DifferentialGeometry/Index.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="Index.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023 Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+using Mathematics.NET.DifferentialGeometry.Abstractions;
+using Mathematics.NET.Symbols;
+
+namespace Mathematics.NET.DifferentialGeometry;
+
+/// <summary>Represents a tensor index</summary>
+/// <typeparam name="T">An index position</typeparam>
+/// <typeparam name="U">A symbol</typeparam>
+public struct Index<T, U> : IIndex
+    where T : IIndexPosition
+    where U : ISymbol
+{
+    public static IIndex Instance => new Index<T, U>();
+
+    //
+    // Formatting
+    //
+
+    public override string ToString() => ToString(null, null);
+
+    public string ToString(string? format, IFormatProvider? provider) => $"{T.DisplayString} {U.DisplayString}";
+}
