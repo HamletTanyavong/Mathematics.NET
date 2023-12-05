29b5867603351d3837ef401659ab9453219f5dc6
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2023-12-04 23:02:04 -0600

    Add benchmarks for Matrix4x4

---
+++ tests/Mathematics.NET.Benchmarks/LinearAlgebra/Matrix4x4TransposeBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="Matrix4x4TransposeBenchmarks.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023 Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+using Mathematics.NET.Benchmarks.Implementations.LinearAlgebra;
+using Mathematics.NET.LinearAlgebra;
+
+namespace Mathematics.NET.Benchmarks.LinearAlgebra;
+
+public class Matrix4x4TransposeBenchmarks
+{
+    public Matrix4x4<Real> A { get; set; }
+
+    public void GlobalSetup()
+    {
+        A = new(
+            1.4, 0.12, 3, 4,
+            5, 6, 7.5, -8,
+            9, 10, 12.1, 12,
+            3.3, 14, 15, 16);
+    }
+
+    [Benchmark(Baseline = true)]
+    public Matrix4x4<Real> TransposeNaive() => A.TransposeNaive();
+
+    [Benchmark]
+    public Matrix4x4<Real> TransposeSimdV1() => A.TransposeSimdV1();
+}
