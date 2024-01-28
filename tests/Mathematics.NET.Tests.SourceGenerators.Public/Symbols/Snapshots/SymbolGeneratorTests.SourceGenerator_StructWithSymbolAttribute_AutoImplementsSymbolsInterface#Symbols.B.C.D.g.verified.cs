8863bcba01bbcb757f99bd482e14697064b56b0a
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-01-28 14:10:36 -0600

    Add test project for public source generator

---
+++ tests/Mathematics.NET.Tests.SourceGenerators.Public/Symbols/Snapshots/SymbolGeneratorTests.SourceGenerator_StructWithSymbolAttribute_AutoImplementsSymbolsInterface#Symbols.B.C.D.g.verified.cs
@@ -0,0 +1,21 @@
+﻿// Auto-generated code
+
+using Mathematics.NET.Symbols;
+
+namespace B.C.D;
+
+public readonly partial struct Zeta : ISymbol
+{
+    /// <inheritdoc cref="ISymbol.DisplayString"/>
+    public const string DisplayString = "zeta";
+
+    static string ISymbol.DisplayString => DisplayString;
+}
+
+public readonly partial struct Eta : ISymbol
+{
+    /// <inheritdoc cref="ISymbol.DisplayString"/>
+    public const string DisplayString = "eta";
+
+    static string ISymbol.DisplayString => DisplayString;
+}
