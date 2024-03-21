80ccaa2c0453a54b79ad0ef5ef8a0e241efb1278
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-03-21 08:08:58 -0500

    Create TensorField4x4.cs

---
+++ src/Mathematics.NET/DifferentialGeometry/TensorField4x4.cs
@@ -0,0 +1,117 @@
+﻿// <copyright file="TensorField4x4.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023 Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+using Mathematics.NET.AutoDiff;
+using Mathematics.NET.Core.Buffers;
+using Mathematics.NET.DifferentialGeometry.Abstractions;
+using Mathematics.NET.LinearAlgebra;
+using Mathematics.NET.Symbols;
+
+namespace Mathematics.NET.DifferentialGeometry;
+
+/// <summary>Represents a rank-two tensor field with 16 elements</summary>
+/// <typeparam name="TTape">A type that implements <see cref="ITape{T}"/></typeparam>
+/// <typeparam name="TNumber">A type that implements <see cref="IComplex{T}"/> and <see cref="IDifferentiableFunctions{T}"/></typeparam>
+/// <typeparam name="TIndex1Position">An index</typeparam>
+/// <typeparam name="TIndex2Position">An index</typeparam>
+/// <typeparam name="TPointIndex">An index</typeparam>
+public class TensorField4x4<TTape, TNumber, TIndex1Position, TIndex2Position, TPointIndex> : TensorField<TNumber, TPointIndex>
+    where TTape : ITape<TNumber>
+    where TNumber : IComplex<TNumber>, IDifferentiableFunctions<TNumber>
+    where TIndex1Position : IIndexPosition
+    where TIndex2Position : IIndexPosition
+    where TPointIndex : IIndex
+{
+    private protected AutoDiffTensor4Buffer4x4<TTape, TNumber, TPointIndex> _buffer;
+
+    public TensorField4x4() { }
+
+    public Func<TTape, AutoDiffTensor4<TNumber, TPointIndex>, Variable<TNumber>> this[int row, int column]
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _buffer[row][column];
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set => _buffer[row][column] = value;
+    }
+
+    public Tensor<Matrix4x4<TNumber>, TNumber, Index<TIndex1Position, TIndex1>, Index<TIndex2Position, TIndex2>> Compute<TIndex1, TIndex2>(TTape tape, AutoDiffTensor4<TNumber, TPointIndex> x)
+        where TIndex1 : ISymbol
+        where TIndex2 : ISymbol
+    {
+        tape.IsTracking = false;
+
+        Matrix4x4<TNumber> result = new();
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (_buffer[i][j] is Func<TTape, AutoDiffTensor4<TNumber, TPointIndex>, Variable<TNumber>> function)
+                {
+                    result[i, j] = function(tape, x).Value;
+                }
+            }
+        }
+
+        tape.IsTracking = true;
+        return new Tensor<Matrix4x4<TNumber>, TNumber, Index<TIndex1Position, TIndex1>, Index<TIndex2Position, TIndex2>>(result);
+    }
+
+    /// <summary>Compute the gradient of all elements of the tensor.</summary>
+    /// <typeparam name="TIndex1Name">The first index of the result tensor</typeparam>
+    /// <typeparam name="TIndex2Name">The second index of the result tensor</typeparam>
+    /// <param name="tape">A gradient or Hessian tape</param>
+    /// <param name="point">A point on the manifold</param>
+    /// <returns>A read-only span of gradients</returns>
+    public ReadOnlySpan<Tensor<Matrix4x4<TNumber>, TNumber, Index<TIndex1Position, TIndex1Name>, Index<TIndex2Position, TIndex2Name>>> ElementGradient<TIndex1Name, TIndex2Name>(
+        TTape tape,
+        AutoDiffTensor4<TNumber, TPointIndex> point)
+        where TIndex1Name : ISymbol
+        where TIndex2Name : ISymbol
+    {
+        Span<Tensor<Matrix4x4<TNumber>, TNumber, Index<TIndex1Position, TIndex1Name>, Index<TIndex2Position, TIndex2Name>>> result = new Tensor<Matrix4x4<TNumber>, TNumber, Index<TIndex1Position, TIndex1Name>, Index<TIndex2Position, TIndex2Name>>[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (this[i, j] is Func<TTape, AutoDiffTensor4<TNumber, TPointIndex>, Variable<TNumber>> function)
+                {
+                    _ = function(tape, point);
+                    tape.ReverseAccumulate(out var gradient);
+
+                    result[0][i, j] = gradient[0];
+                    result[1][i, j] = gradient[1];
+                    result[2][i, j] = gradient[2];
+                    result[3][i, j] = gradient[3];
+                }
+            }
+        }
+        return result;
+    }
+}
