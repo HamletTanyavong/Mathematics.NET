51b9046a478a3e0c806148a52cc5c3646c823c2c
HamletTanyavong <HamletTanyavong@users.noreply.github.com>
2024-03-28 16:03:43 +0000

    deploy: d9bdb09cbf58d9ad339741a514d28c56381dd7ac

---
+++ public/main.js
@@ -0,0 +1,15 @@
+export default {
+  defaultTheme: 'dark',
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/HamletTanyavong/Mathematics.NET',
+      title: 'GitHub'
+    },
+    {
+      icon: 'heart',
+      href: 'https://github.com/sponsors/HamletTanyavong',
+      title: 'Sponsor'
+    }
+  ]
+}
