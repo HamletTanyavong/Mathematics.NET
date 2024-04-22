018eda3225f02c3ccc823ccf1359f8695f4241bf
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-04-22 07:59:25 -0500

    Create AutoDiffTensor3Buffer3x3.cs

---
+++ src/Mathematics.NET/Core/Buffers/AutoDiffTensor3Buffer3x3.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="AutoDiffTensor3Buffer3x3.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023 Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+#pragma warning disable IDE0051
+
+using System.Runtime.CompilerServices;
+using Mathematics.NET.AutoDiff;
+using Mathematics.NET.DifferentialGeometry.Abstractions;
+
+namespace Mathematics.NET.Core.Buffers;
+
+/// <summary>Represents a buffer of 3 AutoDiffTensor3Buffer3 buffers</summary>
+/// <typeparam name="TT">A type that implements <see cref="ITape{T}"/></typeparam>
+/// <typeparam name="TN">A type that implements <see cref="IComplex{T}"/> and <see cref="IDifferentiableFunctions{T}"/></typeparam>
+/// <typeparam name="TPI">The index of the point on the manifold</typeparam>
+[InlineArray(3)]
+internal struct AutoDiffTensor3Buffer3x3<TT, TN, TPI>
+    where TT : ITape<TN>
+    where TN : IComplex<TN>, IDifferentiableFunctions<TN>
+    where TPI : IIndex
+{
+    private AutoDiffTensor3Buffer3<TT, TN, TPI> _element0;
+}
