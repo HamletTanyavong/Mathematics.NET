058b2e0087b1320812dcfddef5913773a2f078c5
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-08-08 07:32:25 -0500

    Create SystemState`4.cs

---
+++ src/Mathematics.NET/Solvers/SystemState`4.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="SystemState`4.cs" company="Mathematics.NET">
+// Mathematics.NET
+// https://github.com/HamletTanyavong/Mathematics.NET
+//
+// MIT License
+//
+// Copyright (c) 2023-present Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+using Mathematics.NET.DifferentialGeometry.Abstractions;
+using Mathematics.NET.LinearAlgebra.Abstractions;
+
+namespace Mathematics.NET.Solvers;
+
+/// <summary>Represents the state of a system.</summary>
+/// <typeparam name="TR1T">A rank-one tensor.</typeparam>
+/// <typeparam name="TV">The backing type of the tensor.</typeparam>
+/// <typeparam name="TN">A type that implements <see cref="IComplex{T}"/> and <see cref="IDifferentiableFunctions{T}"/>.</typeparam>
+/// <typeparam name="TI">The index of the tensor.</typeparam>
+/// <param name="system">The system.</param>
+/// <param name="time">The time.</param>
+public sealed class SystemState<TR1T, TV, TN, TI>(Memory<TR1T> system, TN time)
+    where TR1T : IRankOneTensor<TR1T, TV, TN, TI>
+    where TV : IVector<TV, TN>
+    where TN : IComplex<TN>, IDifferentiableFunctions<TN>
+    where TI : IIndex
+{
+    /// <inheritdoc cref="SystemState{T}.System"/>
+    public Memory<TR1T> System = system;
+
+    /// <inheritdoc cref="SystemState{T}.Time"/>
+    public TN Time = time;
+}
