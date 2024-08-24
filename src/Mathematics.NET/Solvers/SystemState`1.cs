0be432c86e8830a324762cb218d82acac44fc4a9
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-08-24 15:12:08 -0500

    Update SystemState`1.cs
    - Fix copyright header.

--- src/Mathematics.NET/Solvers/SystemState`1.cs
+++ src/Mathematics.NET/Solvers/SystemState`1.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="SystemState.cs" company="Mathematics.NET">
+﻿// <copyright file="SystemState`1.cs" company="Mathematics.NET">
 // Mathematics.NET
 // https://github.com/HamletTanyavong/Mathematics.NET
 //
