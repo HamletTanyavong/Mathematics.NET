437e40aa196b1d788e2b38b4f761113f6dcf992a
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-08-24 14:31:02 -0500

    Remove public field Radix

--- src/Mathematics.NET/Core/Rational.cs
+++ src/Mathematics.NET/Core/Rational.cs
@@ -50,8 +50,6 @@ public readonly struct Rational<T> : IRational<Rational<T>, T>
     public static readonly Rational<T> NegativeInfinity = new(-T.One, T.Zero);
     public static readonly Rational<T> PositiveInfinity = new(T.One, T.Zero);
 
-    public static readonly int Radix = 2;
-
     private readonly T _numerator;
     private readonly T _denominator;
 
@@ -113,7 +111,7 @@ public readonly struct Rational<T> : IRational<Rational<T>, T>
     static Rational<T> IComplex<Rational<T>>.Zero => Zero;
     static Rational<T> IComplex<Rational<T>>.One => One;
     static Rational<T> IComplex<Rational<T>>.NaN => NaN;
-    static int IComplex<Rational<T>>.Radix => Radix;
+    static int IComplex<Rational<T>>.Radix => 2;
     static Rational<T> IMinMaxValue<Rational<T>>.MaxValue => MaxValue;
     static Rational<T> IMinMaxValue<Rational<T>>.MinValue => MinValue;
 
--- src/Mathematics.NET/Core/Real.cs
+++ src/Mathematics.NET/Core/Real.cs
@@ -52,8 +52,6 @@ public readonly struct Real(double real)
     public static readonly Real NegativeInfinity = double.NegativeInfinity;
     public static readonly Real PositiveInfinity = double.PositiveInfinity;
 
-    public static readonly int Radix = 2;
-
     /// <inheritdoc cref="Constants.E" />
     public static readonly Real E = Constants.E;
 
@@ -114,7 +112,7 @@ public readonly struct Real(double real)
     static Real IComplex<Real>.Zero => Zero;
     static Real IComplex<Real>.One => One;
     static Real IComplex<Real>.NaN => NaN;
-    static int IComplex<Real>.Radix => Radix;
+    static int IComplex<Real>.Radix => 2;
     static Real IMinMaxValue<Real>.MaxValue => MaxValue;
     static Real IMinMaxValue<Real>.MinValue => MinValue;
 
