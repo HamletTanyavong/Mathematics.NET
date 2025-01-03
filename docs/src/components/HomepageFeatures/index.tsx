ba0091aa992971c942f82f2ec635c330fd8d512a
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2025-01-02 20:58:43 -0600

    Update index.tsx
    Update home page features.

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -4,38 +4,27 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: <>Docusaurus was designed from the ground up to be easily installed and used to get your website up and running quickly.</>
+    title: 'Mathematical Types',
+    description: <>Use custom types for complex, real, and rational numbers as well as other types for vectors, matrices, and tensors.</>
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the <code>docs</code> directory.
-      </>
-    )
+    title: 'AutoDiff',
+    description: <>Mathematics.NET supports first and second-order, forward and reverse mode automatic differentiation.</>
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: <>Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same header and footer.</>
+    title: 'Differential Geometry',
+    description: <>Use methods for common operations in differential geometry such as index raising, lowering, and contractions.</>
   }
 ];
 
-function Feature({ title, Svg, description }: FeatureItem) {
+function Feature({ title, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
