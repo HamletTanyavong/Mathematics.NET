cce8937245adb624f373e21e525a0a12ccc6eee8
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2025-01-03 16:50:34 -0600

    Add custom theming

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,16 +1,21 @@
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
+import AnimatedGridPattern from '../components/ui/animated-grid-pattern';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <header className={clsx('hero overflow-hidden bg-secondary text-secondary-foreground', styles.heroBanner)}>
+      <AnimatedGridPattern
+        numSquares={32}
+        maxOpacity={0.25}
+        className="inset-y-[-50%] h-[200%] skew-x-6 skew-y-6 [mask-image:radial-gradient(50vw_circle_at_center,white,transparent)]"
+      />
+      <div className="container z-10">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -23,7 +28,9 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`${siteConfig.title}`} description="Mathematics.NET is a C# class library that provides tools for solving advanced mathematical problems; it has support for automatic differentiation (autodiff) as well as operations commonly used in calculus, linear algebra, and differential geometry.">
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Mathematics.NET is a C# class library that provides tools for solving advanced mathematical problems; it has support for automatic differentiation (autodiff) as well as operations commonly used in calculus, linear algebra, and differential geometry.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
