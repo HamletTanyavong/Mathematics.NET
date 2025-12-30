94b1460484e348397966e8130a49ab88035dc7d6
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2025-12-30 11:22:37 -0600

    Move CompVecMulScalar with overwrite to new file

--- src/Mathematics.NET/GPU/OpenCL/OpenCLService.cs
+++ src/Mathematics.NET/GPU/OpenCL/OpenCLService.cs
@@ -163,34 +163,6 @@ public sealed partial class OpenCLService : IComputeService
 
     #region Interface Implementations
 
-    // TODO: Consider putting this in another file.
-
-    public unsafe void CompVecMulScalar(Device device, nuint globalWorkSize, nuint localWorkSize, Span<Complex> vector, in Complex scalar)
-    {
-        fixed (void* pVector = vector)
-        {
-            // Create buffers.
-            nint vectorBuffer = _cl.CreateBuffer(_context.Handle, MemFlags.UseHostPtr | MemFlags.ReadWrite | MemFlags.HostReadOnly, (nuint)(sizeof(Complex) * vector.Length), pVector, null);
-
-            // Set kernel arguments.
-            var kernel = _program.Kernels["comp_vec_mul_scalar_overwrite"].Handle;
-            var error = _cl.SetKernelArg(kernel, 0, (nuint)sizeof(nint), &vectorBuffer);
-            error |= _cl.SetKernelArg(kernel, 1, (nuint)sizeof(Complex), in scalar);
-            ComputeServiceException.ThrowIfCouldNotSetKernelArguments(error, device.Name, "comp_vec_mul_scalar_overwrite");
-
-            // Enqueue NDRange kernel.
-            using var commandQueue = _context.CreateCommandQueue(device, CommandQueueProperties.None);
-            error = _cl.EnqueueNdrangeKernel(commandQueue.Handle, kernel, 1, null, &globalWorkSize, &localWorkSize, 0, null, null);
-            ComputeServiceException.ThrowIfCouldNotEnqueueNDRangeKernel(error, device.Name, "comp_vec_mul_scalar_overwrite");
-
-            // Enqueue read buffer.
-            _cl.EnqueueReadBuffer(commandQueue.Handle, vectorBuffer, true, 0, (nuint)(sizeof(Complex) * vector.Length), pVector, 0, null, null);
-
-            // Release mem objects.
-            _cl.ReleaseMemObject(vectorBuffer);
-        }
-    }
-
     public unsafe ReadOnlySpan<Complex> CompVecMulScalar(Device device, nuint globalWorkSize, nuint localWorkSize, ReadOnlySpan<Complex> vector, in Complex scalar)
     {
         var length = vector.Length;
