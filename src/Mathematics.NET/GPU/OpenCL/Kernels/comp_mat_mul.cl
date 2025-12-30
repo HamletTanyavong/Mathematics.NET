48801bd7a4c2407be0babc01bfac8cacee67a787
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2025-12-30 11:34:33 -0600

    Move CompMatMul to new file

--- src/Mathematics.NET/GPU/OpenCL/Kernels/comp_mat_mul.cl
+++ src/Mathematics.NET/GPU/OpenCL/Kernels/comp_mat_mul.cl
@@ -3,14 +3,15 @@
 __kernel void comp_mat_mul(__global const complex* matA,
                            __global const complex* matB,
                            const int k,
+                           const int width,
                            __global complex* result)
 {
     int row = get_global_id(0);
     int col = get_global_id(1);
-    int width = get_local_size(1) * get_num_groups(1);
 
     int aIndex = row * k;
     int bIndex = col;
+    int cIndex = row * width + col;
 
     complex sum = { .re = 0, .im = 0 };
 
@@ -21,5 +22,5 @@ __kernel void comp_mat_mul(__global const complex* matA,
         bIndex += width;
     }
 
-    result[row * width + col] = sum;
+    result[cIndex] = sum;
 }
--- src/Mathematics.NET/GPU/OpenCL/OpenCLService.cs
+++ src/Mathematics.NET/GPU/OpenCL/OpenCLService.cs
@@ -163,53 +163,6 @@ public sealed partial class OpenCLService : IComputeService
 
     #region Interface Implementations
 
-    public unsafe ReadOnlySpan2D<Complex> CompMatMul(Device device, WorkSize2D globalWorkSize, WorkSize2D localWorkSize, ReadOnlySpan2D<Complex> left, ReadOnlySpan2D<Complex> right)
-    {
-        var k = left.Width;
-        if (k != right.Height)
-            throw new MathematicsException("Cannot multiply two matrices with incompatible dimensions.");
-        Span2D<Complex> result = new Complex[left.Height, right.Width];
-
-        fixed (void* pLeft = left)
-        {
-            // Create buffers.
-            nint leftBuffer = _cl.CreateBuffer(_context.Handle, MemFlags.UseHostPtr | MemFlags.ReadOnly | MemFlags.HostNoAccess, (nuint)(sizeof(Complex) * left.Length), pLeft, null);
-
-            fixed (void* pRight = right)
-            {
-                nint rightBuffer = _cl.CreateBuffer(_context.Handle, MemFlags.UseHostPtr | MemFlags.ReadOnly | MemFlags.HostNoAccess, (nuint)(sizeof(Complex) * right.Length), pRight, null);
-
-                fixed (void* pResult = result)
-                {
-                    nint resultBuffer = _cl.CreateBuffer(_context.Handle, MemFlags.UseHostPtr | MemFlags.WriteOnly | MemFlags.HostReadOnly, (nuint)(sizeof(Complex) * result.Length), pResult, null);
-
-                    // Set kernel arguments.
-                    var kernel = _program.Kernels["comp_mat_mul"].Handle;
-                    var error = _cl.SetKernelArg(kernel, 0, (nuint)sizeof(nint), &leftBuffer);
-                    error |= _cl.SetKernelArg(kernel, 1, (nuint)sizeof(nint), &rightBuffer);
-                    error |= _cl.SetKernelArg(kernel, 2, sizeof(int), &k);
-                    error |= _cl.SetKernelArg(kernel, 3, (nuint)sizeof(nint), &resultBuffer);
-                    ComputeServiceException.ThrowIfCouldNotSetKernelArguments(error, device.Name, "comp_mat_mul");
-
-                    // Enqueue NDRange kernel.
-                    using var commandQueue = _context.CreateCommandQueue(device, CommandQueueProperties.None);
-                    error = _cl.EnqueueNdrangeKernel(commandQueue.Handle, kernel, 2, null, (nuint*)&globalWorkSize, (nuint*)&localWorkSize, 0, null, null);
-                    ComputeServiceException.ThrowIfCouldNotEnqueueNDRangeKernel(error, device.Name, "comp_mat_mul");
-
-                    // Enqueue read buffer.
-                    _cl.EnqueueReadBuffer(commandQueue.Handle, resultBuffer, true, 0, (nuint)(sizeof(Complex) * result.Length), pResult, 0, null, null);
-
-                    // Release mem objects.
-                    _cl.ReleaseMemObject(resultBuffer);
-                }
-                _cl.ReleaseMemObject(rightBuffer);
-            }
-            _cl.ReleaseMemObject(leftBuffer);
-        }
-
-        return result;
-    }
-
     public unsafe ReadOnlySpan2D<Real> MatMul(Device device, WorkSize2D globalWorkSize, WorkSize2D localWorkSize, ReadOnlySpan2D<Real> left, ReadOnlySpan2D<Real> right)
     {
         var k = left.Width;
