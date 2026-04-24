fafed3f1f24a2f341fce4cf8a85d6b398b4c3e4e
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2026-04-24 02:18:46 -0500

    Update HeroPattern.tsx
    Change colors.

--- docs/src/components/HeroPattern.tsx
+++ docs/src/components/HeroPattern.tsx
@@ -4,7 +4,7 @@ export function HeroPattern() {
   return (
     <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
       <div className="absolute top-0 left-1/2 -ml-152 h-100 w-325 dark:mask-[linear-gradient(white,transparent)]">
-        <div className="absolute inset-0 bg-linear-to-r from-[#36b49f] to-[#DBFF75] mask-[radial-gradient(farthest-side_at_top,white,transparent)] opacity-40 dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100">
+        <div className="absolute inset-0 bg-linear-to-r from-[#0000ff] to-[#ffd700] mask-[radial-gradient(farthest-side_at_top,white,transparent)] opacity-40 dark:from-[#0000ff]/30 dark:to-[#ffd700]/30 dark:opacity-100">
           <GridPattern
             width={72}
             height={56}
